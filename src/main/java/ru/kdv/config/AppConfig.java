81e32e50fc4f8df0e20f1ed581ad5c3bc96b22d0
KDanila <concista@gmail.com>
2020-03-25 21:42:54 +0300

    Add AppConfig and controller.

---
+++ src/main/java/ru/kdv/config/AppConfig.java
@@ -0,0 +1,4 @@
+package ru.kdv.config;
+
+public class AppConfig {
+}
---
+++ src/main/java/ru/kdv/controller/AppRestController.java
@@ -0,0 +1,9 @@
+package ru.kdv.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AppRestController {
+}
